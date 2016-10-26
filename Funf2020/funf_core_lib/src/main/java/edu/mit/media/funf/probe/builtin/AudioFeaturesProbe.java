a55d097372790ce78f0702489957ce37c3d66dd7
justin <zhenghx3@asiainfo.com>
2016-10-26 14:18:13 +0800

    监听电话事件，打电话的过程中不采集数据

--- Funf2020/funf_core_lib/src/main/java/edu/mit/media/funf/probe/builtin/AudioFeaturesProbe.java
+++ Funf2020/funf_core_lib/src/main/java/edu/mit/media/funf/probe/builtin/AudioFeaturesProbe.java
@@ -49,34 +49,40 @@ import edu.mit.media.funf.probe.builtin.ProbeKeys.AudioFeaturesKeys;
 @RequiredFeatures("android.hardware.microphone")
 @RequiredPermissions(android.Manifest.permission.RECORD_AUDIO)
 public class AudioFeaturesProbe extends Base implements ContinuousProbe, AudioFeaturesKeys {
+	public final static String TAG = "AudioFeaturesProbe";
+
 
 	// TODO: may need to change this to 44100 sampling to make it more compatible across devices
 	// Alternatively, we could dynamically discover it 
 	// http://stackoverflow.com/questions/6745344/record-audio-using-audiorecord-in-android
 
-	private static int RECORDER_SOURCE = MediaRecorder.AudioSource.VOICE_RECOGNITION;
+	private static int RECORDER_SOURCE = MediaRecorder.AudioSource.MIC;
 	private static int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
 	private static int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
 	private static int RECORDER_SAMPLERATE = 8000;
 
-	private static int FFT_SIZE = 8192;
-	private static int MFCCS_VALUE = 12;
-	private static int MEL_BANDS = 20;
-	private static double[] FREQ_BANDEDGES = {50, 250, 500, 1000, 2000};
+	private static final int AVERAGE_LIMIT = 5;
+	private int count = 0;
+	private double dbSum = 0d;
+
+//	private static int FFT_SIZE = 8192;
+//	private static int MFCCS_VALUE = 12;
+//	private static int MEL_BANDS = 20;
+//	private static double[] FREQ_BANDEDGES = {50, 250, 500, 1000, 2000};
 
 	private Thread recordingThread = null;
 	private int bufferSize = 0;
 	private int bufferSamples = 0;
-	private static int[] freqBandIdx = null;
+//	private static int[] freqBandIdx = null;
 
-	private FFT featureFFT = null;
-	private MFCC featureMFCC = null;
-	private Window featureWin = null;
+//	private FFT featureFFT = null;
+//	private MFCC featureMFCC = null;
+//	private Window featureWin = null;
 
 	private AudioRecord audioRecorder = null;
 
 	public double prevSecs = 0;
-	public double[] featureBuffer = null;
+//	public double[] featureBuffer = null;
 
 	@Override
 	protected void onStart() {
@@ -92,16 +98,16 @@ public class AudioFeaturesProbe extends Base implements ContinuousProbe, AudioFe
 
 
 		//allocateFrameFeatureBuffer(STREAM_FEATURES);
-
-		featureFFT = new FFT(FFT_SIZE);
-		featureWin = new Window(bufferSamples);
-		featureMFCC = new MFCC(FFT_SIZE, MFCCS_VALUE, MEL_BANDS, RECORDER_SAMPLERATE);
-
-		freqBandIdx = new int[FREQ_BANDEDGES.length];
-		for (int i = 0; i < FREQ_BANDEDGES.length; i++) {
-			freqBandIdx[i] = Math.round((float) FREQ_BANDEDGES[i] * ((float) FFT_SIZE / (float) RECORDER_SAMPLERATE));
-			//writeLogTextLine("Frequency band edge " + i + ": " + Integer.toString(freqBandIdx[i]));
-		}
+//
+//		featureFFT = new FFT(FFT_SIZE);
+//		featureWin = new Window(bufferSamples);
+//		featureMFCC = new MFCC(FFT_SIZE, MFCCS_VALUE, MEL_BANDS, RECORDER_SAMPLERATE);
+//
+//		freqBandIdx = new int[FREQ_BANDEDGES.length];
+//		for (int i = 0; i < FREQ_BANDEDGES.length; i++) {
+//			freqBandIdx[i] = Math.round((float) FREQ_BANDEDGES[i] * ((float) FFT_SIZE / (float) RECORDER_SAMPLERATE));
+//			//writeLogTextLine("Frequency band edge " + i + ": " + Integer.toString(freqBandIdx[i]));
+//		}
 
 		audioRecorder = new AudioRecord(
 				RECORDER_SOURCE,
@@ -134,10 +140,10 @@ public class AudioFeaturesProbe extends Base implements ContinuousProbe, AudioFe
 
 	private void handleAudioStream() {
 		short data16bit[] = new short[bufferSamples];
-		byte data8bit[] = new byte[bufferSize];
-		double fftBufferR[] = new double[FFT_SIZE];
-		double fftBufferI[] = new double[FFT_SIZE];
-		double featureCepstrum[] = new double[MFCCS_VALUE];
+//		byte data8bit[] = new byte[bufferSize];
+//		double fftBufferR[] = new double[FFT_SIZE];
+//		double fftBufferI[] = new double[FFT_SIZE];
+//		double featureCepstrum[] = new double[MFCCS_VALUE];
 
 		int readAudioSamples = 0;
 		while (State.RUNNING.equals(getState())) {
@@ -148,73 +154,79 @@ public class AudioFeaturesProbe extends Base implements ContinuousProbe, AudioFe
 
 			JsonObject data = new JsonObject();
 			if (readAudioSamples > 0) {
-				double fN = (double) readAudioSamples;
-
+				double db = caculDB(data16bit, readAudioSamples);
+				Log.i(TAG, "dB-->" + db);
+				// 数据异常重启
+				reStartRecorder(db);
+				Gson gson = getGson();
+				data.add("DB", gson.toJsonTree(db));
+//				double fN = (double) readAudioSamples;
+//
 				data.addProperty(DIFF_SECS, diffSecs);
 
 				// Convert shorts to 8-bit bytes for raw audio output
-				for (int i = 0; i < bufferSamples; i++) {
-					data8bit[i * 2] = (byte) data16bit[i];
-					data8bit[i * 2 + 1] = (byte) (data16bit[i] >> 8);
-				}
+//				for (int i = 0; i < bufferSamples; i++) {
+//					data8bit[i * 2] = (byte) data16bit[i];
+//					data8bit[i * 2 + 1] = (byte) (data16bit[i] >> 8);
+//				}
 				//		        	writeLogTextLine("Read " + readAudioSamples + " samples");
 
-				// L1-norm
-				double accum = 0;
-				for (int i = 0; i < readAudioSamples; i++) {
-					accum += Math.abs((double) data16bit[i]);
-				}
-				data.addProperty(L1_NORM, accum / fN);
-
-				// L2-norm
-				accum = 0;
-				for (int i = 0; i < readAudioSamples; i++) {
-					accum += (double) data16bit[i] * (double) data16bit[i];
-				}
-				data.addProperty(L2_NORM, Math.sqrt(accum / fN));
-
-				// Linf-norm
-				accum = 0;
-				for (int i = 0; i < readAudioSamples; i++) {
-					accum = Math.max(Math.abs((double) data16bit[i]), accum);
-				}
-				data.addProperty(LINF_NORM, Math.sqrt(accum));
+//				// L1-norm
+//				double accum = 0;
+//				for (int i = 0; i < readAudioSamples; i++) {
+//					accum += Math.abs((double) data16bit[i]);
+//				}
+//				data.addProperty(L1_NORM, accum / fN);
+//
+//				// L2-norm
+//				accum = 0;
+//				for (int i = 0; i < readAudioSamples; i++) {
+//					accum += (double) data16bit[i] * (double) data16bit[i];
+//				}
+//				data.addProperty(L2_NORM, Math.sqrt(accum / fN));
+//
+//				// Linf-norm
+//				accum = 0;
+//				for (int i = 0; i < readAudioSamples; i++) {
+//					accum = Math.max(Math.abs((double) data16bit[i]), accum);
+//				}
+//				data.addProperty(LINF_NORM, Math.sqrt(accum));
 
 				// Frequency analysis
-				Arrays.fill(fftBufferR, 0);
-				Arrays.fill(fftBufferI, 0);
+//				Arrays.fill(fftBufferR, 0);
+//				Arrays.fill(fftBufferI, 0);
 
 				// Convert audio buffer to doubles
-				for (int i = 0; i < readAudioSamples; i++) {
-					fftBufferR[i] = data16bit[i];
-				}
+//				for (int i = 0; i < readAudioSamples; i++) {
+//					fftBufferR[i] = data16bit[i];
+//				}
 
 				// In-place windowing
-				featureWin.applyWindow(fftBufferR);
+//				featureWin.applyWindow(fftBufferR);
 
 				// In-place FFT
-				featureFFT.fft(fftBufferR, fftBufferI);
+//				featureFFT.fft(fftBufferR, fftBufferI);
 
 				// Get PSD across frequency band ranges
-				double[] psdAcrossFrequencyBands = new double[FREQ_BANDEDGES.length - 1];
-				for (int b = 0; b < (FREQ_BANDEDGES.length - 1); b++) {
-					int j = freqBandIdx[b];
-					int k = freqBandIdx[b + 1];
-					accum = 0;
-					for (int h = j; h < k; h++) {
-						accum += fftBufferR[h] * fftBufferR[h] + fftBufferI[h] * fftBufferI[h];
-					}
-					psdAcrossFrequencyBands[b] = accum / ((double) (k - j));
-				}
-				Gson gson = getGson();
-				data.add(PSD_ACROSS_FREQUENCY_BANDS, gson.toJsonTree(psdAcrossFrequencyBands));
-
-				// Get MFCCs
-				featureCepstrum = featureMFCC.cepstrum(fftBufferR, fftBufferI);
-				data.add(MFCCS, gson.toJsonTree(featureCepstrum));
+//				double[] psdAcrossFrequencyBands = new double[FREQ_BANDEDGES.length - 1];
+//				for (int b = 0; b < (FREQ_BANDEDGES.length - 1); b++) {
+//					int j = freqBandIdx[b];
+//					int k = freqBandIdx[b + 1];
+//					accum = 0;
+//					for (int h = j; h < k; h++) {
+//						accum += fftBufferR[h] * fftBufferR[h] + fftBufferI[h] * fftBufferI[h];
+//					}
+//					psdAcrossFrequencyBands[b] = accum / ((double) (k - j));
+//				}
+//				Gson gson = getGson();
+//				data.add(PSD_ACROSS_FREQUENCY_BANDS, gson.toJsonTree(psdAcrossFrequencyBands));
+//
+//				// Get MFCCs
+//				featureCepstrum = featureMFCC.cepstrum(fftBufferR, fftBufferI);
+//				data.add(MFCCS, gson.toJsonTree(featureCepstrum));
 
 //				data.add("data16bit", gson.toJsonTree(data16bit));
-				Log.i("AudioFeaturesProbe", "mfccs-->" + Arrays.toString(featureCepstrum));
+//				Log.i("AudioFeaturesProbe", "mfccs-->" + Arrays.toString(featureCepstrum));
 
 				// Write out features
 				sendData(data);
@@ -222,6 +234,38 @@ public class AudioFeaturesProbe extends Base implements ContinuousProbe, AudioFe
 			}
 		}
 
+	}
+
+	private double caculDB(short data16bit[], int sampleBytes) {
+		long v = 0;
+
+		for (int i = 0; i < data16bit.length; i++) {
+			v += data16bit[i] * data16bit[i];
+		}
+		// 平方和除以数据总长度，得到音量大小。
+		double mean = v / (double) sampleBytes;
+		double db = 10 * Math.log10(mean);
+
+		return db;
+	}
+
+	//规避DB基准点突然降低的情况
+	private void reStartRecorder(double db) {
+		dbSum += db;
+		count++;
+		double value;
+		if (count == AVERAGE_LIMIT) {
+			value = dbSum / AVERAGE_LIMIT;
+			Log.i(TAG, "db平均值-->" + value);
+			if (value < 40) {
+				Log.i(TAG, "--重启录音服务--");
+				audioRecorder.stop();
+				audioRecorder.release();
+				onStart();
+			}
+			dbSum = 0d;
+			count = 0;
+		}
 	}
 
 }
